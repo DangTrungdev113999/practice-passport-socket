2f744c5eb9dabc5dafbef5bbeba62256d5e1f91a
dangtrungdev <dangtrungdev113999@gmail.com>
2019-10-31 10:26:54 +0700

    fix bug

---
+++ sh/env.example.sh
@@ -0,0 +1,16 @@
+export APP_HOST=localhost;
+export APP_PORT=3000;
+
+export DB_CONNECT=mongodb;
+export DB_HOST=localhost;
+export DB_PORT=27017;
+export DB_NAME=practice_passport_socket;
+export DB_USERNAME="";
+export DB_PASSWORD="";
+
+export SESSION_KEY="phieuyet";
+export SESSION_SECRET="redCat";
+
+export FB_ID=421407961907034;
+export FB_SECRET=9dc17c024299d31c1746fb99878d0b3e;
+export FB_CALLBACL_URL=https://localhost:3000/auth/facebook/callback;
---
+++ src/public/js/chat.js
@@ -0,0 +1,5 @@
+// $('.nav-tabs a').click(function(){
+//   let targetId = $(this).data("uid"); 
+//   $(`.nav-tabs a[data-uid="${targetId}"]`).tab('show');
+//   console.log(targetId);
+// })
